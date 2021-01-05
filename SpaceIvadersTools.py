3bd33098bc9762309adf6031e002849b644c0d11
hanna384 <hannalbala@gmail.com>
2021-01-05 12:40:09 +0100

    création d'un ennemi

---
+++ SpaceIvadersTools.py
@@ -0,0 +1,22 @@
+# le 5 janvier 2021
+# programme écrit par Hanna Albala
+#classes, méthodes et fonctions
+
+
+from tkinter import *
+import random
+
+
+# classe des objets ennemies
+#avec une position horizontal X et vertical Y
+#avec une méthode de déplacement
+class ennemis:
+
+    def __init__(self):
+        self.posX=random.randint(0,655)
+        self.posY=random.randint(0,200)
+        
+
+
+
+
--- main.py
+++ main.py
@@ -2,14 +2,46 @@
 # programme écrit par Hanna Albala
 # lien gitHub    https://github.com/hanna384/TP3Python.git
 # ToDo : 
+# faire en sorte que l'ennemi bouge tout seul
+#puis ajouter plusieurs ennemis
+#mais surtout il faut d'abord que l'affichage de l'ennemi se fasse dans une methode de la classe et pas dans le programme principal car sinon ça va être trop dur a gerer
 
-from tkinter import Tk, Label, Button, PhotoImage, Canvas
-#from tkinter import *
-from functionsSI import *
+#from tkinter import Tk, Label, Button, PhotoImage, Canvas, Menu
+from tkinter import *
+from SpaceIvadersTools import *
+
+
+
+
+
+
+def mooveHorizontal(ennemi):
+    #incremente coordonnées de la balle
+    ennemi.posX += 20
+
+    #rappel de mooveHorizontal toutes les 50ms
+    ennemi.after(50, ennemi.mooveHorizontal)
+
+#fonction principal du jeu
+def new_game():
+    nombreEnnemis=6
+
+    ennemi_1= ennemis()
+    
+    #r rayon du cercle ennemi
+    r=45
+    #cercle de centre (posX,posY) et de rayon r
+    monCanvas.create_oval(ennemi_1.posX, ennemi_1.posY, ennemi_1.posX+r, ennemi_1.posY+r, outline='red', fill='red')
+    print (monCanvas.find_all())
+
+    mooveHorizontal(ennemi_1)
+
+
 
 myWindow = Tk()
 myWindow.title('Space Invaders')
 myWindow.geometry('800x500')
+
 
 labelScore = Label(myWindow, text = "Score : ")
 labelScore.grid(row=0, column=0)
@@ -20,17 +52,31 @@ labelLives.grid(row=0, column=1)
 #labelLives.pack(side= 'top')
 
 photo = PhotoImage(file = "terre.gif")
-Largeur = photo.height() 
-Hauteur = photo.width()
-monCanvas = Canvas(myWindow, width = 700, height =445)
+largeur = 700 
+hauteur = 445
+monCanvas = Canvas(myWindow, width = largeur, height =hauteur)
 item = monCanvas.create_image(0,0 , anchor ='nw' , image = photo)
 monCanvas.grid(row=1, column= 0)
 
-buttonNewGame = Button (myWindow, text="New Game", command = myWindow.destroy)
+buttonNewGame = Button (myWindow, text="New Game", command = new_game)
 buttonNewGame.grid(row=1, column=1)
 
 buttonQuit = Button (myWindow, text="Quitter", command = myWindow.destroy)
 buttonQuit.grid(row=2, column=1)
+
+#création du menu
+# menu = Menu(myWindow) 
+# monMenu = Menu(menu, tearoff = 0)   
+# monMenu.add_command(label="Quit", command = myWindow.destroy) 
+# monMenu.add_command(label="TEST", command = myWindow.destroy)  
+  
+# myWindow.config(menu = menu) 
+
+
+
+
+
+
 
 
 myWindow.mainloop()
