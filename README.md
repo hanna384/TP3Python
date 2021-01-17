6b94208b560c5493d830fe34338ccfe68c25d079
hanna384 <hannalbala@gmail.com>
2021-01-17 22:02:38 +0100

    ajout NB

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # TP3Python
+# le 5 janvier 2021
+# programme écrit par Hanna Albala
+# NB: étant absente au premier TP, j'ai travaillée seule et pas en binome, le jeu n'est pas complet mais bien avancé
+# lien gitHub    https://github.com/hanna384/TP3Python.git
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 # le 5 janvier 2021
 # programme écrit par Hanna Albala
-# NB: étant absente au premier TP, j'ai travaillée seule et pas en binome, le jeu n'est pas complet mais bien avancé
 # lien gitHub    https://github.com/hanna384/TP3Python.git
 # ToDo : 
 # Lorsqu'un missile rencontre un ennemie, les deux doivents etre détruit
 # gerer les vies restantes et le score
+#ameliorer les fonctions d'affichage photo fond
 
 from tkinter import Tk, Label, Button, PhotoImage, Canvas, Menu
 #from tkinter import *
