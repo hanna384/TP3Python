d937f7cc5e015c581e044fc09a7a5fa521620a9f
hanna384 <hannalbala@gmail.com>
2021-01-17 22:00:06 +0100

    ajout NB

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # le 5 janvier 2021
 # programme écrit par Hanna Albala
+# NB: étant absente au premier TP, j'ai travaillée seule et pas en binome, le jeu n'est pas complet mais bien avancé
 # lien gitHub    https://github.com/hanna384/TP3Python.git
 # ToDo : 
 # Lorsqu'un missile rencontre un ennemie, les deux doivents etre détruit
